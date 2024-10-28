dbad743395fccf7830673ce575e421221320417e
SuperXRanzo <okanskyavenger@gmail.com>
2024-10-28 20:43:49 +0700

    Menghitung Keliling

--- CreateClass/CreateClass.py
+++ CreateClass/CreateClass.py
@@ -2,3 +2,6 @@ class PersegiPanjang:
     def _init_(self, panjang, lebar):
         self.panjang = panjang
         self.lebar = lebar
+        
+    def hitung_keliling(self):
+        return 2 * (self.panjang + self.lebar)
